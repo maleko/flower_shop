0ff8b31ff9f3a1126b444d19786eaeb7782cb09d
Marc Lee <marc@adviso.com.au>
2015-04-23 23:24:57 +1000

    flowers

--- lib/flower_shop/flower.rb
+++ lib/flower_shop/flower.rb
@@ -1,5 +1,47 @@
-class FlwoerShop::Flower
+class FlowerShop::Flower
 
-  attr_reader :name, :code, :available_bundles
+  attr_accessor :name, :code, :available_bundles
+
+  def initialize name, code, available_bundles
+    @name              = name
+    @code              = code
+    @available_bundles = available_bundles
+  end
+
+  def sorted_bundle_sizes
+    @sorted_bundle_sizes ||= available_bundles.keys.sort{ |a,b| b<=> a }
+  end
+
+  def calculate_cost(bundles)
+    bundles.inject(0) { |sum, bundle| sum += available_bundles[bundle] }
+  end
+
+  # http://rubyquiz.com/quiz154.html
+  def select_bundles(flowers_required)
+     optimum_combo = Hash.new do |hash, key|
+       hash[key] = if key < sorted_bundle_sizes.min
+         []
+       elsif sorted_bundle_sizes.include?(key)
+         [key]
+       else
+         sorted_bundle_sizes.
+           # prune unhelpful bundles
+           reject { |bundle| bundle > key }.
+
+           # prune bundles that are factors of larger bundles
+           inject([]) {|mem, var| mem.any? {|c| c%var == 0} ? mem : mem+[var]}.
+
+           # recurse
+           map { |bundle| [bundle] + hash[key - bundle] }.
+
+           # prune unhelpful solutions
+           reject { |bundle| bundle.inject(:+) != key }.
+
+           # pick the smallest, empty if none
+           min { |a, b| a.size <=> b.size } || []
+       end
+     end
+     optimum_combo[flowers_required]
+  end
 
 end
--- spec/lib/flower_shop/flower_spec.rb
+++ spec/lib/flower_shop/flower_spec.rb
@@ -2,4 +2,114 @@ require 'spec_helper'
 
 describe FlowerShop::Flower do
 
+  let(:flower)         { FlowerShop::Flower.new("Orchid", "O100", bundle) }
+
+  describe "#sorted_bundle_sizes" do
+
+    let(:flower)         { FlowerShop::Flower.new("Orchid", "O100", bundle) }
+    let(:expected_order) { [15, 9, 4] }
+
+    subject      { flower.sorted_bundle_sizes }
+
+    context "bundle order" do
+      let(:bundle) { {9 => "8.00", 4 => "5.00", 15 => "12.00"} }
+
+      it 'returns the bundle sorted' do
+        expect(subject).to eql expected_order
+      end
+    end
+
+    context "bundle order" do
+      let(:bundle) { {4 => "5.00", 15 => "12.00", 9 => "8.00"} }
+
+      it 'returns the bundle sorted' do
+        expect(subject).to eql expected_order
+      end
+    end
+
+  end
+
+  describe "#calculate_cost" do
+
+    let(:bundle) { {4 => 5.00, 15 => 12.00, 9 => 8.00} }
+
+    subject      { flower.calculate_cost(required_flowers) }
+
+    context "one small bundle only" do
+      let(:required_flowers) { [4] }
+
+      it 'returns the cost' do
+        expect(subject).to eq bundle[4]
+      end
+    end
+
+    context "two bundles of the same size" do
+      let(:required_flowers) { [4, 4] }
+
+      it 'returns the cost' do
+        expect(subject).to eq( bundle[4] * 2 )
+      end
+    end
+
+    context "complicated bundles of one each" do
+      let(:required_flowers) { [4, 15, 9] }
+
+      it 'returns the cost' do
+        expect(subject).to eq( bundle[4] + bundle[15] + bundle[9] )
+      end
+    end
+
+  end
+
+  describe "#select_bundles" do
+
+    subject { flower.select_bundles(required_flowers) }
+
+    context "simple bundle" do
+
+      let(:bundle) { {4 => 5.00, 15 => 12.00, 9 => 8.00} }
+
+      context "smallest bundle only" do
+        let(:required_flowers) { 4 }
+
+        it 'returns the required bundle size' do
+          expect(subject).to eq [4]
+        end
+      end
+
+      context "two of the same bundles" do
+        let(:required_flowers) { 8 }
+
+        it 'returns the required bundle size' do
+          expect(subject).to eq [4, 4]
+        end
+      end
+
+      context "complicated bundle" do
+        let(:required_flowers) { 24 }
+
+        it 'returns the required bundle size' do
+          expect(subject).to eq [15, 9]
+        end
+      end
+
+      context "too small a bundle" do
+        let(:required_flowers) { 3 }
+
+        it 'returns nil' do
+          expect(subject).to eq []
+        end
+      end
+
+      context "unfulfillable bundle" do
+        let(:required_flowers) { 5 }
+
+        it 'returns nil' do
+          expect(subject).to eq []
+        end
+      end
+    end
+
+  end
+
 end
